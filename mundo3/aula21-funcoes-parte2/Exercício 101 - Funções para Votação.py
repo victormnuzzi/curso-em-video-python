ed70fa9359291c5f94a2f9336614aa9457938ee6
Victor <victormnuzzi003@gmail.com>
2023-05-09 19:06:30 -0300

    Nome aula23 corrigido

--- mundo3/aula21-funcoes-parte2/Exercício 101 - Funções para Votação.py
+++ mundo3/aula21-funcoes-parte2/Exercício 101 - Funções para Votação.py
@@ -1,3 +1,16 @@
 """
-FUPQ tenha uma função chamada voto() que vai receber como prâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
+FUPQ tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 """
+
+def voto(ano_nasc):
+    idade = 2023 - ano_nasc
+    if idade >= 18:
+        print('Você deve votar.')
+    elif 18 > idade >= 16:
+        print('Voce já pode votar.')
+    else:
+        print('Você ainda não pode votar') 
+
+voto(int(input('Digite o seu ano de nascimento: ')))
+
+# %%
