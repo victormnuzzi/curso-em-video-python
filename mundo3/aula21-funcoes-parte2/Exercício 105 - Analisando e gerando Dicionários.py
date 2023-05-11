f0e342606817a8ae73895d9ff79a222d4d8fba55
Victor <victormnuzzi003@gmail.com>
2023-05-11 10:57:42 -0300

    Exercício 104

--- mundo3/aula21-funcoes-parte2/Exercício 105 - Analisando e gerando Dicionários.py
+++ mundo3/aula21-funcoes-parte2/Exercício 105 - Analisando e gerando Dicionários.py
@@ -1,5 +1,5 @@
 """
-FUPQ tenha uma função notas() que pode receber várias notsa de alunos e vai retornar um dicionário com as seguintes informações:
+FUPQ tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
 - Quantidade de notas
 - A maior nota
 - A menor nota
