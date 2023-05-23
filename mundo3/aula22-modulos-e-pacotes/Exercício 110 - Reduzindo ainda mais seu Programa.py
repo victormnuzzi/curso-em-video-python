23fd312efa5c563a032564dda4c07c846dda939e
Victor <victormnuzzi003@gmail.com>
2023-05-23 20:06:02 -0300

    Exercício 110

--- mundo3/aula22-modulos-e-pacotes/Exercício 110 - Reduzindo ainda mais seu Programa.py
+++
@@ -1,3 +0,0 @@
-"""
-Adicione um módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(), que mnostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
-"""
--- mundo3/aula22-modulos-e-pacotes/Exercício 110 - Reduzindo ainda mais seu Programa/moeda.py
+++ mundo3/aula22-modulos-e-pacotes/Exercício 110 - Reduzindo ainda mais seu Programa/moeda.py
@@ -8,7 +8,7 @@ def diminuir(preco=0, taxa=0, formato=False):
     return res if formato is False else moeda(res)
 
 
-def dobro(preço=0,formato=False):
+def dobro(preço=0, formato=False):
     res = preço * 2
     return res if formato is False else moeda(res)
 
@@ -17,7 +17,19 @@ def metade(preco=0, formato=False):
     res = preco / 2
     return res if formato is False else moeda(res)
 
+
 def moeda(preco=0, moeda='R$'):
     return f'{moeda}{preco:.2f}'.replace('.',',')
 
+
+def resumo(preco=0, taxaa=10, taxar=5):
+    print('-'*36)
+    print('RESUMO DO VALOR'.center(36))
+    print('-'*36)
+    print(f'Preço analisado: \t{moeda(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'{taxaa}% de aumento: \t{aumentar(preco, taxaa, True)}')
+    print(f'{taxar}% de redução: \t\t{diminuir(preco, taxar, False)}')
+    print('-'*36)
 
