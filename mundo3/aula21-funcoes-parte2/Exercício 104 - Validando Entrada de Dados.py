44b0278bfaa347dc46a7d3e52b9ca31fed4b2e0c
Victor <victormnuzzi003@gmail.com>
2023-05-27 20:48:58 -0300

    Correção Exercício 104

--- mundo3/aula21-funcoes-parte2/Exercício 104 - Validando Entrada de Dados.py
+++ mundo3/aula21-funcoes-parte2/Exercício 104 - Validando Entrada de Dados.py
@@ -2,15 +2,15 @@
 FUPQ tenha uma função leiaInt(), que vai funcionar de semelhante à função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.
 Ex: n = leiaInt('Digite um n)
 """
-# Função para ler números inteiros
-def leiaInt(num):
+
+def leiaInt(frase=''):
     '''
     -> Ler um texto e verificar se é um número inteiro
     :param num: O número escolhido pelo usuário
     :return: O número digitado pelo usuário na forma de inteiro
     '''
     while True:
-        num = str(input('Digite um número: '))
+        num = str(input(frase))
         if not num.isnumeric(): # se o num não for numérico, print do erro
             print('\033[31mERRO! Digite um número válido.\033[m') 
         else: 
