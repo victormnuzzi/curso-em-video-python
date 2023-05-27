c73cc4ae3635f2c175a0a1d30d7737fdfb6691b8
Victor <victormnuzzi003@gmail.com>
2023-05-27 00:24:01 -0300

    Exercício 111

---
+++ mundo3/aula22-modulos-e-pacotes/Exercício 111 - Tranformando Módulos em Pacotes/teste.py
@@ -0,0 +1,9 @@
+"""
+Crie um pacote chamado utilidadecev que tenha dois módulos internos chamados moeda e dado.
+Transfira todas as funções utilizadas nos Exercícios 107, 108, 109 e 110 para o primeiro pacote e mantenha tudo funcionando.
+"""
+
+from utilidadescev import moeda
+ 
+p = float(input('Digite o preço: R$'))
+moeda.resumo(p)
