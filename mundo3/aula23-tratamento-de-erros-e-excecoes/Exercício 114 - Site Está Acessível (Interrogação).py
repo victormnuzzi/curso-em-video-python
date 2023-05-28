2d6af842291033b6596bb9f2c4fd0a021c9fe849
Victor <victormnuzzi003@gmail.com>
2023-05-27 23:39:22 -0300

    Correção no commit anterior: Exercício 113* | Exercício 114

--- mundo3/aula23-tratamento-de-erros-e-excecoes/Exercício 114 - Site Está Acessível (Interrogação).py
+++ mundo3/aula23-tratamento-de-erros-e-excecoes/Exercício 114 - Site Está Acessível (Interrogação).py
@@ -1,3 +1,44 @@
 """
 Crie um código em Python que teste se o site Pudim está accesível pelo computador usado.
 """
+
+# Minha resolução:
+"""
+import requests # tive que dar pip install requests
+
+def url_ok(url):
+    '''
+    -> Verificar se um site está acessível
+    :param url: A url digitada pelo usuário
+    :return: Se o site está está acessível
+    '''
+    try:
+        status = requests.head(url).status_code
+    except:
+        return 'O site Pudim não está disponível no momento.'
+    else:
+        return 'Consegui acessar o site Pudim com sucesso!'
+    
+    
+print(url_ok('http://pudim.com.br/'))
+"""
+
+# Resolução do Guanabara
+
+import urllib.request # biblioteca já instalada no python 
+
+def url_ok(url):
+    '''
+    -> Verificar se um site está acessível
+    :param url: A url digitada pelo usuário
+    :return: Se o site está está acessível
+    '''
+    try:
+        site = urllib.request.urlopen('http://pudim.com.br/')
+    except urllib.error.URLError:
+        return 'O site Pudim não está disponível no momento.'
+    else:
+        return 'Consegui acessar o site Pudim com sucesso!'
+
+    
+print(url_ok('http://pudim.com.br/'))
